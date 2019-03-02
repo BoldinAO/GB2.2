7ed33cb2131a8e65b08b6f48fe33257422d09e98
Anatoliy <boldinao@icloud.com>
2019-03-02 23:08:00 +0600

    Выполнено ДЗ 2 PS без xml описания

---
+++ HW2/HW2/Workers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace HW2
+{
+    class Workers
+    {
+        public Workers() => WorkersList = new List<BaseWorker>();
+
+        public List<BaseWorker> WorkersList { get; }
+
+        public void AddWorker(BaseWorker worker)
+        {
+            WorkersList.Add(worker);
+        }
+
+        public IEnumerable GetWorkers
+        {
+            get
+            {
+                for(int i = 0; i < WorkersList.Count; i++) yield return WorkersList[i];
+            }
+        }
+    }
+}
